21c5c0c841ff42ad59c4e527e2ef90c352276c64
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-07 21:23:40 +0530

    [Ritesh]add : seprate Indivisible charcter From The String

---
+++ BasicPractice/SeprateChar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPractice
+{
+    public class SeprateChar
+    {
+        public void Input()
+        {
+            Console.WriteLine("Enter A string");
+            string Name = Console.ReadLine();
+            int i = 0;
+            while (i <= Name.Length - 1)
+            {
+                Console.Write("{0} ", Name[i]);
+                i++;
+            }
+        }
+    }
+}
